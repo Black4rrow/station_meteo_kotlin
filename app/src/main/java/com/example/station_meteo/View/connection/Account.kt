d8c17b7c286239476bec8c5cc23ad7fd9712212f
royeryannis <royeryannis@gmail.com>
2025-03-11 11:25:53 +0100

    fix : Confict merge

--- app/src/main/java/com/example/station_meteo/View/connection/Account.kt
+++ app/src/main/java/com/example/station_meteo/View/connection/Account.kt
@@ -1,8 +1,6 @@
 package com.example.station_meteo.View.connection
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,7 +13,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
@@ -25,10 +22,6 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -37,14 +30,13 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.station_meteo.R
-
 import com.example.station_meteo.ViewModel.UserViewModel
 import com.google.firebase.auth.FirebaseAuth
 
 @Composable
 fun Account(navController: NavController) {
 
-    val  userViewModel: UserViewModel = viewModel()
+    val userViewModel: UserViewModel = viewModel()
     val name = userViewModel.getUserMail()?.let { userViewModel.transformMailInName(it) }
 
     Box(
@@ -76,7 +68,7 @@ fun Account(navController: NavController) {
 
 
             if (name != null) {
-                Text(text = name , style = TextStyle(fontSize = 24.sp))
+                Text(text = name, style = TextStyle(fontSize = 24.sp))
             }
             TextButton(onClick = {
                 FirebaseAuth.getInstance().signOut()
@@ -99,7 +91,11 @@ fun Account(navController: NavController) {
                     AccountInfoCard(label = "Name of account", value = name, showMailIcon = false)
                 }
                 Spacer(modifier = Modifier.height(8.dp))
-                AccountInfoCard(label = "Mot de passe", value = "*************", showMailIcon = false)
+                AccountInfoCard(
+                    label = "Mot de passe",
+                    value = "*************",
+                    showMailIcon = false
+                )
             }
         }
     }
